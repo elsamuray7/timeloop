a1626877007aabfe7ca8ac902ffaf87972011f26
Samuel <fixed-term.Samuel.Holderbach@de.bosch.com>
2021-12-15 08:35:25 +0100

    added possibility to specify initial delay

--- timeloop/app.py
+++ timeloop/app.py
@@ -20,8 +20,8 @@ class Timeloop():
         logger.setLevel(logging.INFO)
         self.logger = logger
 
-    def _add_job(self, func, interval, *args, **kwargs):
-        j = Job(interval, func, *args, **kwargs)
+    def _add_job(self, func, interval, initial_delay, *args, **kwargs):
+        j = Job(interval, func, initial_delay, *args, **kwargs)
         self.jobs.append(j)
 
     def _block_main_thread(self):
@@ -46,9 +46,9 @@ class Timeloop():
             self.logger.info("Stopping job {}".format(j.execute))
             j.stop()
 
-    def job(self, interval):
+    def job(self, interval, initial_delay=None):
         def decorator(f):
-            self._add_job(f, interval)
+            self._add_job(f, interval, initial_delay)
             return f
         return decorator
 
--- timeloop/job.py
+++ timeloop/job.py
@@ -2,10 +2,11 @@ from threading import Thread, Event
 from datetime import timedelta
 
 class Job(Thread):
-    def __init__(self, interval, execute, *args, **kwargs):
+    def __init__(self, interval, execute, initial_delay, *args, **kwargs):
         Thread.__init__(self)
         self.stopped = Event()
         self.interval = interval
+        self.delay = initial_delay or timedelta(seconds=0)
         self.execute = execute
         self.args = args
         self.kwargs = kwargs
@@ -15,5 +16,7 @@ class Job(Thread):
         self.join()
 
     def run(self):
+        if not self.stopped.wait(self.delay.total_seconds()):
+            self.execute(*self.args, **self.kwargs)
         while not self.stopped.wait(self.interval.total_seconds()):
             self.execute(*self.args, **self.kwargs)
